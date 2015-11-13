9937a1bff4d884b2ef300d102f802d426c793f6d
Joseph Saied William <jswilliam93@gmail.com>
2015-11-13 08:14:49 +0200

    joe's commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,15 @@ class User < ActiveRecord::Base
   has_many :comments
   validates :name, presence: true
   validates :password, presence: true, length: { minimum: 5 }
-  validates :email, presence: true, uniqueness: true
+
+  class EmailValidator < ActiveModel::EachValidator
+		def validate_each(record, attribute, value)
+			unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+				record.errors[attribute] << (options[:message] || "is not a valid email")
+			end
+		end
+	end
+
+  validates :email, presence: true, uniqueness: true, email: true
   validates :sid, uniqueness: true, allow_nil: true, allow_blank: true
 end
